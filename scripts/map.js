2fd2dce54ae14eda8200a55cc9bd05f5bf37abe5
EthanGJFrench <ethanfrenc@gmail.com>
2026-05-18 23:39:50 +1200

    Added select menu UI for filtering trees

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,14 @@
-var map = L.map('map').setView([-43.532, 172.636], 12);
+// Init map
+var map = L.map('map', { zoomControl: false }).setView([-43.532, 172.636], 12);
 
+// Add zoom controls to bottom right of page
+L.control.zoom({
+    position: 'bottomright'
+}).addTo(map);
+
+// Add OpenStreetMap tile
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 15,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    minZoom: 10,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
