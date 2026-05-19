36d73b777625a8430481e32b31e6558aaef9eb76
EthanGJFrench <ethanfrenc@gmail.com>
2026-05-19 13:26:21 +1200

    Map page has a dropdown
    - dropdown does not yet work
    - am still styling it

--- scripts/map.js
+++ scripts/map.js
@@ -12,3 +12,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 10,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+// class InteractiveMap {
+//     constructor() {
+         
+//     }
+// }
