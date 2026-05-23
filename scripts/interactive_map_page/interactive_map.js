c336c7402b24ec41a3dd589d93088a914d427802
EthanGJFrench <ethanfrenc@gmail.com>
2026-05-23 17:14:44 +1200

    How renders different color markers for different trees

--- scripts/interactive_map_page/interactive_map.js
+++ scripts/interactive_map_page/interactive_map.js
@@ -69,18 +69,154 @@ export default class InteractiveMap {
         const [LNG, LAT] = tree.geometry.coordinates; // get tree cordernates
 
         switch (this.normaliseString(tree.properties.CommonName)) {// add markers based on 
-            case this.normaliseString("apple"): {
+            case this.normaliseString("apple"): { // if apple
                 const MARKER = L.circleMarker([LAT, LNG], { 
-                    radius: 1,
+                    radius: 3,
                     color: "red"
                 }).addTo(this.map)
                 this.treeMarkers.push(MARKER)
                 break
             }
+
+            case this.normaliseString("apricot"): { // if apricot
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "orange"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("almond"): { // if almond
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "tan"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("crabapple"): { // if crabapple
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "crimson"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("chestnut"): { // if chestnut
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "saddlebrown"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("cherry"): { // if cherry
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "deeppink"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
             
+            case this.normaliseString("europeanbeech"): { // if europeanbeech
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "darkgreen"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("elderberry"): { // if elderberry
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "purple"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("honeylocust"): { // if honeylocust
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "gold"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+            
+            case this.normaliseString("loquat"): { // if loquat
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "coral"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("olive"): { // if olive
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "olive"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("peach"): { // if peach
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "blue"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("pear"): { // if pear
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "yellowgreen"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("plum"): { // if plum
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "darkviolet"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("quince"): { // if peach
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    // color: "lightblue"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
+            case this.normaliseString("walnut"): { // if walnut
+                const MARKER = L.circleMarker([LAT, LNG], { 
+                    radius: 3,
+                    color: "brown"
+                }).addTo(this.map)
+                this.treeMarkers.push(MARKER)
+                break
+            }
+
             default: {
                 const MARKER = L.circleMarker([LAT, LNG], {
-                    radius: 1,
+                    radius: 3,
+                    color: "grey"
                 }).addTo(this.map)
                 break
             }
